3aeb08656632f088e9ba648c8c8b2f3bc4043a2e
nxh177@case.edu <nxh177@case.edu>
2015-10-26 00:33:35 -0400

    completed for sunday night
    Signed-off-by: nxh177@case.edu <nxh177@case.edu>

--- my_interesting_moves/src/baxter_flex.cpp
+++ my_interesting_moves/src/baxter_flex.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv) {
         ros::init(argc, argv, "traj_action_client_node"); 
         ros::NodeHandle nh;         
         int g_count = 0;
-        int ans;
+        
 
         Vectorq7x1 q_flex;
         q_flex<< -0.907528, -0.111813,   2.06622,    1.8737,    -1.295,   2.00164,  -2.87179;
@@ -31,8 +31,8 @@ int main(int argc, char** argv) {
         //initially empty trajectory
         trajectory_msgs::JointTrajectory des_trajectory;
 
-        cout<<"Instantiating a new trajectory (enter) 1"<<endl;
-        cin>> ans;
+        cout<<"Instantiating a new trajectory "<<endl;
+        
 
         //this is where you instantiate a new right arm move by passing in a node handle
         My_interesting_moves right_arm_moves(&nh);
@@ -45,17 +45,63 @@ int main(int argc, char** argv) {
        	}
 
        	cout<<"getting current right-arm pose:"<<endl;
-	    q_vec_right_arm =  baxter_traj_streamer.get_qvec_right_arm();  
+	    q_vec_right_arm =  right_arm_moves.get_qvec_right_arm();  
 	    cout<<"r_arm state:"<<q_vec_right_arm.transpose()<<endl;    
 	    q_in_vecxd = q_vec_right_arm; // start from here;
 	    des_path.push_back(q_in_vecxd); //put all zeros here
 	    q_in_vecxd = q_flex; // conversion; not sure why I needed to do this...but des_path.push_back(q_in_vecxd) likes it
 	    des_path.push_back(q_in_vecxd); //twice, to define a trajectory 
 
-	    
+	    cout << "stuffing traj: " << endl;
+    	right_arm_moves.stuff_trajectory(des_path, des_trajectory); //convert from vector of 7dof poses to trajectory message        
+        // here is a "goal" object compatible with the server, as defined in example_action_server/action
+        right_arm_moves::trajGoal goal; 
+        // does this work?  copy traj to goal:
+        goal.trajectory = des_trajectory;
+        
+        actionlib::SimpleActionClient<right_arm_moves::trajAction> action_client("trajActionServer", true);
+        
+        // attempt to connect to the server:
+        ROS_INFO("waiting for server: ");
+        bool server_exists = action_client.waitForServer(ros::Duration(5.0)); // wait for up to 5 seconds
+        // something odd in above: does not seem to wait for 5 seconds, but returns rapidly if server not running
 
 
-    }        
+        if (!server_exists) {
+            ROS_WARN("could not connect to server; will wait forever");
+            return 0; // bail out; optionally, could print a warning message and retry
+        }
+        server_exists = action_client.waitForServer(); //wait forever 
+        
+       
+        ROS_INFO("Connection to Action Server exists");  
+
+        
+        g_count++;
+        goal.traj_id = g_count; // this merely sequentially numbers the goals sent
+        ROS_INFO("sending traj_id %d",g_count);
+        //action_client.sendGoal(goal); // simple example--send goal, but do not specify callbacks
+        action_client.sendGoal(goal,&doneCb); // we could also name additional callback functions here, if desired
+        //    action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb); //e.g., like this
+        
+        bool finished_before_timeout = action_client.waitForResult(ros::Duration(5.0));
+        
+        if (!finished_before_timeout) {
+            ROS_WARN("giving up waiting on result for goal number %d",g_count);
+            return 0;
+        }
+        else {
+            ROS_INFO("finished before timeout");
+        }
+        
+        
+
+    return 0;
+}
+
+
+
+        
 
 
 
